448f76b3e08665e7b340bd2eab9df2a2e57b69f0
CPSoft.us <danny.uff@gmail.com>
2024-08-16 14:55:12 -0400

    Changed logo to CNN.

--- DLImageTest/ContentView.swift
+++ DLImageTest/ContentView.swift
@@ -1,24 +1,32 @@
-//
-//  ContentView.swift
-//  DLImageTest
-//
-//  Created by Dan Uff on 8/16/24.
-//
-
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+            
+            Link(destination: URL(string: "https://www.cpsoft.us/")!) {
+																								
+            AsyncImage(url: URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgT-R52bE5nFi11FvXv3Er0ADTmXuBd3ieeQ&s")) { image in
+									image.resizable()
+									.scaledToFit()
+									.padding()
+									
+} placeholder: {
+	
+		Image(systemName: "arrow.2.circlepath.circle")
+		.resizable()
+		.scaledToFit()
+		.padding()
+		.foregroundColor(.gray)
+}
+			
 }
 
+					Spacer()
+}
+}
+}
 #Preview {
     ContentView()
 }
